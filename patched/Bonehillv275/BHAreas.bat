8b49e5aab5d85c9d0d42857c1e13b98001bb6433
omni-axa <kgoon007@gmail.com>
2010-12-23 09:29:14 +0530

    patched stuff

---
+++ patched/Bonehillv275/BHAreas.bat
@@ -0,0 +1,273 @@
+@ECHO OFF
+copy Bonehillv275\tisunpack.exe tisunpack.exe
+tisunpack -s -h -o BH-TIS1\BH0100.tis BoneHillv275\TIZ\BH0100.tiz
+tisunpack -s -h -o BH-TIS1\BH0200.tis BoneHillv275\TIZ\BH0200.tiz
+tisunpack -s -h -o BH-TIS1\BH0300.tis BoneHillv275\TIZ\BH0300.tiz
+tisunpack -s -h -o BH-TIS1\BH0302.tis BoneHillv275\TIZ\BH0302.tiz
+tisunpack -s -h -o BH-TIS1\BH0400.tis BoneHillv275\TIZ\BH0400.tiz
+tisunpack -s -h -o BH-TIS1\BH0401.tis BoneHillv275\TIZ\BH0401.tiz
+tisunpack -s -h -o BH-TIS1\BH0402.tis BoneHillv275\TIZ\BH0402.tiz
+tisunpack -s -h -o BH-TIS1\BH0403.tis BoneHillv275\TIZ\BH0403.tiz
+tisunpack -s -h -o BH-TIS1\BH0500.tis BoneHillv275\TIZ\BH0500.tiz
+tisunpack -s -h -o BH-TIS1\BH0507.tis BoneHillv275\TIZ\BH0507.tiz
+tisunpack -s -h -o BH-TIS1\BH0600.tis BoneHillv275\TIZ\BH0600.tiz
+tisunpack -s -h -o BH-TIS1\BH0700.tis BoneHillv275\TIZ\BH0700.tiz
+tisunpack -s -h -o BH-TIS1\BH1000.tis BoneHillv275\TIZ\BH1000.tiz
+tisunpack -s -h -o BH-TIS1\BH1004.tis BoneHillv275\TIZ\BH1004.tiz
+tisunpack -s -h -o BH-TIS1\BH1100.tis BoneHillv275\TIZ\BH1100.tiz
+tisunpack -s -h -o BH-TIS1\BH1101.tis BoneHillv275\TIZ\BH1101.tiz
+tisunpack -s -h -o BH-TIS1\BH1200.tis BoneHillv275\TIZ\BH1200.tiz
+
+tisunpack -s -h -o BH-TIS2\BH1300.tis BoneHillv275\TIZ\BH1300.tiz
+tisunpack -s -h -o BH-TIS2\BH2000.tis BoneHillv275\TIZ\BH2000.tiz
+tisunpack -s -h -o BH-TIS2\BH2001.tis BoneHillv275\TIZ\BH2001.tiz
+tisunpack -s -h -o BH-TIS2\BH2002.tis BoneHillv275\TIZ\BH2002.tiz
+tisunpack -s -h -o BH-TIS2\BH2005.tis BoneHillv275\TIZ\BH2005.tiz
+tisunpack -s -h -o BH-TIS2\BH2006.tis BoneHillv275\TIZ\BH2006.tiz
+tisunpack -s -h -o BH-TIS2\BH2008.tis BoneHillv275\TIZ\BH2008.tiz
+tisunpack -s -h -o BH-TIS2\BH2010.tis BoneHillv275\TIZ\BH2010.tiz
+tisunpack -s -h -o BH-TIS2\BH2012.tis BoneHillv275\TIZ\BH2012.tiz
+tisunpack -s -h -o BH-TIS2\BH2018.tis BoneHillv275\TIZ\BH2018.tiz
+tisunpack -s -h -o BH-TIS2\BH2019.tis BoneHillv275\TIZ\BH2019.tiz
+tisunpack -s -h -o BH-TIS2\BH2020.tis BoneHillv275\TIZ\BH2020.tiz
+tisunpack -s -h -o BH-TIS2\BH2021.tis BoneHillv275\TIZ\BH2021.tiz
+tisunpack -s -h -o BH-TIS2\BH2022.tis BoneHillv275\TIZ\BH2022.tiz
+tisunpack -s -h -o BH-TIS2\BH2025.tis BoneHillv275\TIZ\BH2025.tiz
+tisunpack -s -h -o BH-TIS2\BH2100.tis BoneHillv275\TIZ\BH2100.tiz
+tisunpack -s -h -o BH-TIS2\BH2101.tis BoneHillv275\TIZ\BH2101.tiz
+tisunpack -s -h -o BH-TIS2\BH2102.tis BoneHillv275\TIZ\BH2102.tiz
+tisunpack -s -h -o BH-TIS2\BH2103.tis BoneHillv275\TIZ\BH2103.tiz
+tisunpack -s -h -o BH-TIS2\BH2104.tis BoneHillv275\TIZ\BH2104.tiz
+tisunpack -s -h -o BH-TIS2\BH2105.tis BoneHillv275\TIZ\BH2105.tiz
+tisunpack -s -h -o BH-TIS2\BH2106.tis BoneHillv275\TIZ\BH2106.tiz
+tisunpack -s -h -o BH-TIS2\BH2107.tis BoneHillv275\TIZ\BH2107.tiz
+tisunpack -s -h -o BH-TIS2\BH2108.tis BoneHillv275\TIZ\BH2108.tiz
+tisunpack -s -h -o BH-TIS2\BH2109.tis BoneHillv275\TIZ\BH2109.tiz
+tisunpack -s -h -o BH-TIS2\BH2110.tis BoneHillv275\TIZ\BH2110.tiz
+tisunpack -s -h -o BH-TIS2\BH2111.tis BoneHillv275\TIZ\BH2111.tiz
+tisunpack -s -h -o BH-TIS2\BH2200.tis BoneHillv275\TIZ\BH2200.tiz
+tisunpack -s -h -o BH-TIS2\BH2201.tis BoneHillv275\TIZ\BH2201.tiz
+tisunpack -s -h -o BH-TIS2\BH2202.tis BoneHillv275\TIZ\BH2202.tiz
+tisunpack -s -h -o BH-TIS2\BH2301.tis BoneHillv275\TIZ\BH2301.tiz
+del tisunpack.exe
+
+xcopy /q /y BoneHillv275\sounds\*.ogg BH-REST\*.*
+copy BoneHillv275\oggdec.exe BH-REST\*.*
+cd BH-REST
+oggdec *.ogg
+del *.ogg
+del oggdec.exe
+cd..
+
+weidu --out BH-REST --biff-get-rest AR0334.WED AR0334SR.BMP AR0334LM.BMP AR0334HT.BMP
+weidu --out BH-REST --biff-get-rest AM0335.WAV AM0500K.WAV AM0508.WAV AM0700E1.WAV AM0700E2.WAV AM0700E3.WAV AM0706.WAV AM0806.WAV
+weidu --out BH-REST --biff-get-rest AM1000.WAV AM1100A1.WAV AM1100A2.WAV AM1100A3.WAV AM1100A4.WAV AM1100A5.WAV AM1100A6.WAV AM1100A7.WAV AM1100A8.WAV AM1100A9.WAV
+weidu --out BH-REST --biff-get-rest AM1300.WAV AM1300N.WAV AM1301.WAV AM1304.WAV AM1304N.WAV AM1306.WAV AM1600B1.WAV AM6003.WAV AMB_E46A.WAV AMB_E46B.WAV
+weidu --out BH-REST --biff-get-rest GAFIREP1.WAV GAHOSMAG.WAV GASMITH.WAV
+
+
+:: move override\BH*.* BH-REST
+:: move override\1003MS1.BAM BH-REST
+:: move override\1007MS1.BAM BH-REST
+:: move override\1008MS1.BAM BH-REST
+:: move override\25SPELL.dlg BH-REST
+:: move override\9101MS1.bam BH-REST
+:: move override\9101MS2.bam BH-REST
+:: move override\9105MS1.BAM BH-REST
+:: move override\9106MS1.BAM BH-REST
+:: move override\9108MS1.BAM BH-REST
+:: move override\9711MS1.BAM BH-REST
+:: move override\9714MS1.BAM BH-REST
+:: move override\9714MS2.BAM BH-REST
+:: move override\9714MS3.BAM BH-REST
+:: move override\9714MS4.BAM BH-REST
+:: move override\9717MS1.BAM BH-REST
+:: move override\9717MS2.BAM BH-REST
+:: move override\AR6526.BCS  BH-REST 2> Null
+:: move override\AR7223.bcs  BH-REST 2> Null
+:: move override\AR7800.bcs  BH-REST 2> Null
+:: move override\AR9300.are  BH-REST 2> Null
+:: move override\AR9300.bcs  BH-REST 2> Null
+:: move override\AR9302.bcs  BH-REST 2> Null
+:: move override\ARAMIS.BAM  BH-REST
+:: move override\BALDUR.BCS  BH-REST
+:: move override\BERSERKR.ITM BH-REST
+:: move override\BFABIO.dlg  BH-REST 2> Null
+:: move override\BGARRI.dlg  BH-REST 2> Null
+:: move override\BMINSC.dlg  BH-REST 2> Null
+:: move override\BOULDER.ITM BH-REST
+:: move override\BOULDER.PRO BH-REST
+:: move override\BRANWJ.dlg  BH-REST 2> Null
+:: move override\cbhghan1.bam BH-REST
+:: move override\CBHGHAND.BAM BH-REST
+:: move override\COSPREM.BAM BH-REST
+:: move override\DUDOORE.bcs BH-REST
+:: move override\DUDOORW.bcs BH-REST
+:: move override\DUTRIGE.bcs BH-REST
+:: move override\DUTRIGW.bcs BH-REST
+:: move override\DYNAJ.dlg   BH-REST 2> Null
+:: move override\EDWINJ.dlg  BH-REST 2> Null
+:: move override\GARRIJ.dlg  BH-REST 2> Null
+:: move override\GIANTSTN.BAM BH-REST
+:: move override\HOBGO2.CRE   BH-REST
+:: move override\ibhghand.bam BH-REST
+:: move override\IBOULDER.BAM BH-REST
+:: move override\ICBCL039.BAM BH-REST
+:: move override\ICLCKBS.BAM  BH-REST
+:: move override\IKEYDUNG.BAM BH-REST
+:: move override\IMISC4F.bam  BH-REST
+:: move override\IMOEN2J.dlg  BH-REST 2> Null
+:: move override\INTERDIA.2DA BH-REST
+:: move override\IOSPREM.BAM  BH-REST
+:: move override\itemdial.2da BH-REST
+:: move override\JAHEIRAJ.dlg BH-REST 2> Null
+:: move override\JELLYCO.CRE  BH-REST
+:: move override\KAGAIJ.dlg   BH-REST 2> Null
+:: move override\KHALIJ.dlg   BH-REST 2> Null
+:: move override\KIVANJ.dlg   BH-REST 2> Null
+:: move override\mastarea.2da BH-REST
+:: move override\MINSCJ.dlg   BH-REST 2> Null
+:: move override\MLI3A1.BAM   BH-REST
+:: move override\MLI3A1E.BAM  BH-REST
+:: move override\MLI3A2.BAM   BH-REST
+:: move override\MLI3A2E.BAM  BH-REST
+:: move override\MLI3CA.BAM   BH-REST
+:: move override\MLI3CAE.BAM  BH-REST
+:: move override\MLI3DE.BAM   BH-REST
+:: move override\MLI3DEE.BAM  BH-REST
+:: move override\MLI3GH.BAM   BH-REST
+:: move override\MLI3GHE.BAM  BH-REST
+:: move override\MLI3GU.BAM   BH-REST
+:: move override\MLI3GUE.BAM  BH-REST
+:: move override\MLI3SC.BAM   BH-REST
+:: move override\MLI3SCE.BAM  BH-REST
+:: move override\MLI3SD.BAM   BH-REST
+:: move override\MLI3SDE.BAM  BH-REST
+:: move override\MLI3SL.BAM   BH-REST
+:: move override\MLI3SLE.BAM  BH-REST
+:: move override\MLI3SP.BAM   BH-REST
+:: move override\MLI3SPE.BAM  BH-REST
+:: move override\MLI3TW.BAM   BH-REST
+:: move override\MLI3TWE.BAM  BH-REST
+:: move override\MLI3WK.BAM   BH-REST
+:: move override\MLI3WKE.BAM  BH-REST
+:: move override\MLIZCA1.BAM  BH-REST
+:: move override\MLIZCA1E.BAM BH-REST
+:: move override\MLIZCA2.BAM  BH-REST
+:: move override\MLIZCA2E.BAM BH-REST
+:: move override\MLIZCCA.BAM  BH-REST
+:: move override\MLIZCCAE.BAM BH-REST
+:: move override\MLIZCGH.BAM  BH-REST
+:: move override\MLIZCGHE.BAM BH-REST
+:: move override\MLIZCGU.BAM  BH-REST
+:: move override\MLIZCGUE.BAM BH-REST
+:: move override\MLIZCSC.BAM  BH-REST
+:: move override\MLIZCSCE.BAM BH-REST
+:: move override\MLIZCSD.BAM  BH-REST
+:: move override\MLIZCSDE.BAM BH-REST
+:: move override\MLIZCSL.BAM  BH-REST
+:: move override\MLIZCSLE.BAM BH-REST
+:: move override\MLIZCSP.BAM  BH-REST
+:: move override\MLIZCSPE.BAM BH-REST
+:: move override\MLIZCWK.BAM  BH-REST
+:: move override\MLIZCWKE.BAM BH-REST
+:: move override\MLIZHA1.BAM  BH-REST
+:: move override\MLIZHA1E.BAM BH-REST
+:: move override\MLIZHA2.BAM  BH-REST
+:: move override\MLIZHA2E.BAM BH-REST
+:: move override\MLIZHCA.BAM  BH-REST
+:: move override\MLIZHCAE.BAM BH-REST
+:: move override\MLIZHGH.BAM  BH-REST
+:: move override\MLIZHGHE.BAM BH-REST
+:: move override\MLIZHGU.BAM  BH-REST
+:: move override\MLIZHGUE.BAM BH-REST
+:: move override\MLIZHSC.BAM  BH-REST
+:: move override\MLIZHSCE.BAM BH-REST
+:: move override\MLIZHSD.BAM  BH-REST
+:: move override\MLIZHSDE.BAM BH-REST
+:: move override\MLIZHSL.BAM  BH-REST
+:: move override\MLIZHSLE.BAM BH-REST
+:: move override\MLIZHSP.BAM  BH-REST
+:: move override\MLIZHSPE.BAM BH-REST
+:: move override\MLIZHWK.BAM  BH-REST
+:: move override\MLIZHWKE.BAM BH-REST
+:: move override\MLIZQA1.BAM  BH-REST
+:: move override\MLIZQA1E.BAM BH-REST
+:: move override\MLIZQA2.BAM  BH-REST
+:: move override\MLIZQA2E.BAM BH-REST
+:: move override\MLIZQCA.BAM  BH-REST
+:: move override\MLIZQCAE.BAM BH-REST
+:: move override\MLIZQGH.BAM  BH-REST
+:: move override\MLIZQGHE.BAM BH-REST
+:: move override\MLIZQGU.BAM  BH-REST
+:: move override\MLIZQGUE.BAM BH-REST
+:: move override\MLIZQSC.BAM  BH-REST
+:: move override\MLIZQSCE.BAM BH-REST
+:: move override\MLIZQSD.BAM  BH-REST
+:: move override\MLIZQSDE.BAM BH-REST
+:: move override\MLIZQSL.BAM  BH-REST
+:: move override\MLIZQSLE.BAM BH-REST
+:: move override\MLIZQSP.BAM  BH-REST
+:: move override\MLIZQSPE.BAM BH-REST
+:: move override\MLIZQWK.BAM  BH-REST
+:: move override\MLIZQWKE.BAM BH-REST
+:: move override\MONTAJ.dlg   BH-REST 2> Null
+:: move override\NETRIG1.bcs  BH-REST
+:: move override\NPCLEVEL.2DA BH-REST
+:: move override\NWTRIG1.bcs  BH-REST
+:: move override\PDIALOG.2DA  BH-REST
+:: move override\PROJECTL.IDS BH-REST
+:: move override\SAFANJ.dlg   BH-REST 2> Null
+:: move override\SETRIG1.bcs  BH-REST
+:: move override\SHARTJ.dlg   BH-REST 2> Null
+:: move override\SWTRIG1.bcs  BH-REST
+:: move override\TOOLTIP.2DA  BH-REST
+:: move override\XANJ.dlg     BH-REST 2> Null
+:: move override\xhlm015.bam  BH-REST
+:: move override\XZARJ.dlg    BH-REST 2> Null
+
+:: move override\GNOLL02.CRE    BH-REST
+:: move override\GNOLL03.CRE    BH-REST
+:: move override\GNOLL3.CRE    BH-REST
+:: move override\SKELET02.CRE    BH-REST
+
+
+:: move override\FW3402.ARE   BH-REST 2> Null
+:: move override\FW3400.ARE   BH-REST 2> Null
+:: move override\_XZARJ.DLG   BH-REST 2> Null
+:: move override\_xANJ.DLG    BH-REST 2> Null
+:: move override\_SHARTJ.DLG  BH-REST 2> Null
+:: move override\_SAFANJ.DLG  BH-REST 2> Null
+:: move override\_MONTAJ.DLG  BH-REST 2> Null
+:: move override\_MINSCJ.DLG  BH-REST 2> Null
+:: move override\_KIVANJ.DLG  BH-REST 2> Null
+:: move override\_KHALIJ.DLG  BH-REST 2> Null
+:: move override\_KAGAIJ.DLG  BH-REST 2> Null
+:: move override\_JAHEIJ.DLG  BH-REST 2> Null
+:: move override\_IMOEN2.DLG  BH-REST 2> Null
+:: move override\_GARRIJ.DLG  BH-REST 2> Null
+:: move override\_EDWINJ.DLG  BH-REST 2> Null
+:: move override\_DYNAHJ.DLG  BH-REST 2> Null
+:: move override\_BRANWJ.DLG  BH-REST 2> Null
+:: move override\_BMINSC.DLG  BH-REST 2> Null
+:: move override\_BGARRI.DLG  BH-REST 2> Null
+:: move override\_AR3402.BCS  BH-REST 2> Null
+:: move override\_AR3400.BCS  BH-REST 2> Null
+:: move override\_AR2626.BCS  BH-REST 2> Null
+:: move override\_AR0800.BCS  BH-REST 2> Null
+:: move override\_AR0123.BCS  BH-REST 2> Null
+
+weidu --make-biff BH-TIS1
+weidu --make-biff BH-TIS2
+weidu --make-biff BH-REST
+
+call BoneHillv275\deldir BH-TIS1
+call BoneHillv275\deldir BH-TIS2
+call BoneHillv275\deldir BH-REST
+
+FOR %%s IN (
+override\*.baf
+override\*.d
+override\*.ogg
+override\*.tiz
+) DO del %%s
---
+++ patched/CtB/CtbFinishInstall.bat
@@ -0,0 +1,185 @@
+@echo off
+
+copy CtB\tisunpack.exe tisunpack.exe
+tisunpack -s -h -o CTB-TIS1/AR3520.tis CtB\TIZ/AR3520.TIZ
+tisunpack -s -h -o CTB-TIS1/AR3525.tis CtB\TIZ/AR3525.TIZ
+tisunpack -s -h -o CTB-TIS1/AR3526.tis CtB\TIZ/AR3526.TIZ
+tisunpack -s -h -o CTB-TIS1/AR3534.tis CtB\TIZ/AR3534.TIZ
+tisunpack -s -h -o CTB-TIS1/AR3535.tis CtB\TIZ/AR3535.TIZ
+tisunpack -s -h -o CTB-TIS1/AR3536.tis CtB\TIZ/AR3536.TIZ
+tisunpack -s -h -o CTB-TIS1/AR3540.tis CtB\TIZ/AR3540.TIZ
+tisunpack -s -h -o CTB-TIS1/AR3541.tis CtB\TIZ/AR3541.TIZ
+tisunpack -s -h -o CTB-TIS1/AR3542.tis CtB\TIZ/AR3542.TIZ
+tisunpack -s -h -o CTB-TIS1/AR3543.tis CtB\TIZ/AR3543.TIZ
+tisunpack -s -h -o CTB-TIS1/AR3544.tis CtB\TIZ/AR3544.TIZ
+tisunpack -s -h -o CTB-TIS1/AR3545.tis CtB\TIZ/AR3545.TIZ
+tisunpack -s -h -o CTB-TIS1/AR3546.tis CtB\TIZ/AR3546.TIZ
+tisunpack -s -h -o CTB-TIS1/AR3547.tis CtB\TIZ/AR3547.TIZ
+tisunpack -s -h -o CTB-TIS1/AR3548.tis CtB\TIZ/AR3548.TIZ
+tisunpack -s -h -o CTB-TIS1/AR3549.tis CtB\TIZ/AR3549.TIZ
+tisunpack -s -h -o CTB-TIS1/AR3550.tis CtB\TIZ/AR3550.TIZ
+tisunpack -s -h -o CTB-TIS1/AR3551.tis CtB\TIZ/AR3551.TIZ
+tisunpack -s -h -o CTB-TIS1/AR3552.tis CtB\TIZ/AR3552.TIZ
+tisunpack -s -h -o CTB-TIS1/AR3553.tis CtB\TIZ/AR3553.TIZ
+tisunpack -s -h -o CTB-TIS1/AR3554.tis CtB\TIZ/AR3554.TIZ
+tisunpack -s -h -o CTB-TIS1/AR3555.tis CtB\TIZ/AR3555.TIZ
+tisunpack -s -h -o CTB-TIS1/AR3556.tis CtB\TIZ/AR3556.TIZ
+tisunpack -s -h -o CTB-TIS1/AR3557.tis CtB\TIZ/AR3557.TIZ
+tisunpack -s -h -o CTB-TIS1/AR3558.tis CtB\TIZ/AR3558.TIZ
+tisunpack -s -h -o CTB-TIS1/AR3559.tis CtB\TIZ/AR3559.TIZ
+tisunpack -s -h -o CTB-TIS2/AR3560.tis CtB\TIZ/AR3560.TIZ
+tisunpack -s -h -o CTB-TIS2/AR3570.tis CtB\TIZ/AR3570.TIZ
+tisunpack -s -h -o CTB-TIS2/AR3575.tis CtB\TIZ/AR3575.TIZ
+tisunpack -s -h -o CTB-TIS2/AR3578.tis CtB\TIZ/AR3578.TIZ
+tisunpack -s -h -o CTB-TIS2/AR3579.tis CtB\TIZ/AR3579.TIZ
+tisunpack -s -h -o CTB-TIS2/AR3580.tis CtB\TIZ/AR3580.TIZ
+tisunpack -s -h -o CTB-TIS2/AR3581.tis CtB\TIZ/AR3581.TIZ
+tisunpack -s -h -o CTB-TIS2/AR3582.tis CtB\TIZ/AR3582.TIZ
+tisunpack -s -h -o CTB-TIS2/AR3583.tis CtB\TIZ/AR3583.TIZ
+tisunpack -s -h -o CTB-TIS2/AR3584.tis CtB\TIZ/AR3584.TIZ
+tisunpack -s -h -o CTB-TIS2/AR3585.tis CtB\TIZ/AR3585.TIZ
+tisunpack -s -h -o CTB-TIS2/AR3586.tis CtB\TIZ/AR3586.TIZ
+tisunpack -s -h -o CTB-TIS2/AR3587.tis CtB\TIZ/AR3587.TIZ
+tisunpack -s -h -o CTB-TIS2/AR3588.tis CtB\TIZ/AR3588.TIZ
+tisunpack -s -h -o CTB-TIS2/AR3590.tis CtB\TIZ/AR3590.TIZ
+tisunpack -s -h -o CTB-TIS2/AR3591.tis CtB\TIZ/AR3591.TIZ
+tisunpack -s -h -o CTB-TIS2/AR3594.tis CtB\TIZ/AR3594.TIZ
+tisunpack -s -h -o CTB-TIS2/AR3597.tis CtB\TIZ/AR3597.TIZ
+tisunpack -s -h -o CTB-TIS2/AR3598.tis CtB\TIZ/AR3598.TIZ
+tisunpack -s -h -o CTB-TIS2/AR3599.tis CtB\TIZ/AR3599.TIZ
+tisunpack -s -h -o CTB-TIS3/AR3600.tis CtB\TIZ/AR3600.TIZ
+tisunpack -s -h -o CTB-TIS3/AR3601.tis CtB\TIZ/AR3601.TIZ
+tisunpack -s -h -o CTB-TIS3/AR3610.tis CtB\TIZ/AR3610.TIZ
+tisunpack -s -h -o CTB-TIS3/AR3610N.tis CtB\TIZ/AR3610N.TIZ
+tisunpack -s -h -o CTB-TIS3/AR3611.tis CtB\TIZ/AR3611.TIZ
+tisunpack -s -h -o CTB-TIS3/AR3611N.tis CtB\TIZ/AR3611N.TIZ
+tisunpack -s -h -o CTB-TIS3/AR3613.tis CtB\TIZ/AR3613.TIZ
+tisunpack -s -h -o CTB-TIS3/AR3613N.tis CtB\TIZ/AR3613N.TIZ
+tisunpack -s -h -o CTB-TIS3/AR3675.tis CtB\TIZ/AR3675.TIZ
+tisunpack -s -h -o CTB-TIS3/AR3616.tis CtB\TIZ/AR3616.TIZ
+tisunpack -s -h -o CTB-TIS3/AR3617.tis CtB\TIZ/AR3617.TIZ
+tisunpack -s -h -o CTB-TIS3/AR3618.tis CtB\TIZ/AR3618.TIZ
+tisunpack -s -h -o CTB-TIS3/AR3619.tis CtB\TIZ/AR3619.TIZ
+tisunpack -s -h -o CTB-TIS3/AR3620.tis CtB\TIZ/AR3620.TIZ
+tisunpack -s -h -o CTB-TIS3/AR3621.tis CtB\TIZ/AR3621.TIZ
+tisunpack -s -h -o CTB-TIS3/AR3622.tis CtB\TIZ/AR3622.TIZ
+tisunpack -s -h -o CTB-TIS3/AR3623.tis CtB\TIZ/AR3623.TIZ
+tisunpack -s -h -o CTB-TIS3/AR3624.tis CtB\TIZ/AR3624.TIZ
+tisunpack -s -h -o CTB-TIS3/AR3625.tis CtB\TIZ/AR3625.TIZ
+tisunpack -s -h -o CTB-TIS3/AR3626.tis CtB\TIZ/AR3626.TIZ
+tisunpack -s -h -o CTB-TIS3/AR3627.tis CtB\TIZ/AR3627.TIZ
+tisunpack -s -h -o CTB-TIS3/AR3628.tis CtB\TIZ/AR3628.TIZ
+tisunpack -s -h -o CTB-TIS3/AR3629.tis CtB\TIZ/AR3629.TIZ
+tisunpack -s -h -o CTB-TIS3/AR3630.tis CtB\TIZ/AR3630.TIZ
+tisunpack -s -h -o CTB-TIS3/AR3632.tis CtB\TIZ/AR3632.TIZ
+tisunpack -s -h -o CTB-TIS3/AR3633.tis CtB\TIZ/AR3633.TIZ
+tisunpack -s -h -o CTB-TIS3/AR3634.tis CtB\TIZ/AR3634.TIZ
+tisunpack -s -h -o CTB-TIS3/AR3635.tis CtB\TIZ/AR3635.TIZ
+tisunpack -s -h -o CTB-TIS3/AR3636.tis CtB\TIZ/AR3636.TIZ
+tisunpack -s -h -o CTB-TIS3/AR3637.tis CtB\TIZ/AR3637.TIZ
+tisunpack -s -h -o CTB-TIS3/AR3638.tis CtB\TIZ/AR3638.TIZ
+tisunpack -s -h -o CTB-TIS3/AR3640.tis CtB\TIZ/AR3640.TIZ
+tisunpack -s -h -o CTB-TIS3/AR3641.tis CtB\TIZ/AR3641.TIZ
+tisunpack -s -h -o CTB-TIS3/AR3642.tis CtB\TIZ/AR3642.TIZ
+tisunpack -s -h -o CTB-TIS3/AR3643.tis CtB\TIZ/AR3643.TIZ
+tisunpack -s -h -o CTB-TIS3/AR3644.tis CtB\TIZ/AR3644.TIZ
+tisunpack -s -h -o CTB-TIS3/AR3645.tis CtB\TIZ/AR3645.TIZ
+tisunpack -s -h -o CTB-TIS3/AR3646.tis CtB\TIZ/AR3646.TIZ
+tisunpack -s -h -o CTB-TIS3/AR3650.tis CtB\TIZ/AR3650.TIZ
+tisunpack -s -h -o CTB-TIS3/AR3651.tis CtB\TIZ/AR3651.TIZ
+tisunpack -s -h -o CTB-TIS3/AR3652.tis CtB\TIZ/AR3652.TIZ
+tisunpack -s -h -o CTB-TIS3/CB3661.tis CtB\TIZ/CB3661.TIZ
+tisunpack -s -h -o CTB-TIS3/CB3662.tis CtB\TIZ/CB3662.TIZ
+tisunpack -s -h -o CTB-TIS3/CB3663.tis CtB\TIZ/CB3663.TIZ
+tisunpack -s -h -o CTB-ARE/CB3664.tis CtB\TIZ/CB3664.TIZ
+tisunpack -s -h -o CTB-BAM/CB3665.tis CtB\TIZ/CB3665.TIZ
+tisunpack -s -h -o CTB-BCS/CB3666.tis CtB\TIZ/CB3666.TIZ
+tisunpack -s -h -o CTB-CRE/CB3667.tis CtB\TIZ/CB3667.TIZ
+tisunpack -s -h -o CTB-ITM/CB3668.tis CtB\TIZ/CB3668.TIZ
+del tisunpack.exe
+
+xcopy /q /y CtB\sound\*.* CTB-WAV\*.*
+
+move override\*.ogg CTB-WAV
+copy CtB\oggdec.exe CTB-WAV\*.*
+cd CTB-WAV
+oggdec *.ogg
+del *.ogg
+del oggdec.exe
+cd..
+
+xcopy /q /e /y CTB\music\*.* music\*.*
+
+:: move override\*.ARE CTB-ARE
+move override\*.WED CTB-ARE
+weidu --make-biff CTB-ARE
+call CtB\deldir CTB-ARE
+
+move override\*.BAM CTB-BAM
+weidu --make-biff CTB-BAM
+call CtB\deldir CTB-BAM
+
+:: move override\*.BCS CTB-BCS
+weidu --make-biff CTB-BCS
+call CtB\deldir CTB-BCS
+
+:: move override\*.CRE CTB-CRE
+:: move override\*.DLG CTB-CRE
+weidu --make-biff CTB-CRE
+call CtB\deldir CTB-CRE
+call CtB\deldir CTBDLG
+
+:: move override\*.ITM CTB-ITM
+weidu --make-biff CTB-ITM
+call CtB\deldir CTB-ITM
+
+move override\*.MOS CTB-MOS
+move override\*.BMP CTB-MOS
+weidu --make-biff CTB-MOS
+call CtB\deldir CTB-MOS
+
+:: move override\*.2DA CTB-RULE
+:: move override\*.CH* CTB-RULE
+::move override\*.IDS CTB-RULE
+:: move override\*.WMP CTB-RULE
+:: move override\*.EFF CTB-RULE
+:: move override\*.PRO CTB-RULE
+:: move override\*.VVC CTB-RULE
+:: move override\*.SPL CTB-RULE
+:: move override\*.STO CTB-RULE
+move override\*.WAV CTB-RULE
+weidu --make-biff CTB-RULE
+call CtB\deldir CTB-RULE
+
+weidu --make-biff CTB-TIS1
+call CtB\deldir CTB-TIS1
+
+weidu --make-biff CTB-TIS2
+call CtB\deldir CTB-TIS2
+
+weidu --out CTB-TIS3 --biff-get-rest AM1700.*WAV
+ren CTB-TIS3\AM1700.WAV AM3675.WAV
+ren CTB-TIS3\AM1700N.WAV AM3675N.WAV
+weidu --make-biff CTB-TIS3
+call CtB\deldir CTB-TIS3
+
+move override\*.MVE CTB-WAV
+weidu --make-biff CTB-WAV
+call CtB\deldir CTB-WAV
+
+FOR %%s IN (
+override\*.baf
+override\*.d
+override\*.ogg
+override\*.tiz
+) DO del %%s
+
+@echo.
+@echo Installation of the core CtB package is complete.
+@echo.
+pause
+
+Setup-CtB-Chores.exe
+
+notepad CtB\text\CtB-Version.txt
+
